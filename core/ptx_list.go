17475436f8fa2cb3d2db732dd620b90b65c6dbdd
ckmonkey35 <ckmonkey35@gmail.com>
2021-03-05 16:51:25 +0800

    feature: ptxReadyMap

--- core/ptx_list.go
+++ core/ptx_list.go
@@ -1,5 +1,7 @@
 package core
 
+import "github.com/ethereum/go-ethereum/core/types"
+
 // indexHeap is a heap.Interface implementation over 64bit unsigned integers for
 // retrieving sorted transactions from the possibly gapped future queue.
 type indexHeap []uint64
@@ -19,4 +21,33 @@ func (h *indexHeap) Pop() interface{} {
 func (h *indexHeap) Push(x interface{}) {
 	*h = append(*h, x.(uint64))
 }
+
+
+type ptxReadyMap struct {
+	items map[uint64]*types.Transaction // Hash map storing the transaction data
+	index *indexHeap                    // Heap of nonces of all the stored transactions (non-strict mode)
+	cache types.Transactions            // Cache of the transactions already sorted
+}
+
+// newPtxReadyMap creates a new index-sorted transaction map.
+func newPtxReadyMap() *ptxReadyMap {
+	return &ptxReadyMap{
+		items: make(map[uint64]*types.Transaction),
+		index: new(indexHeap),
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
