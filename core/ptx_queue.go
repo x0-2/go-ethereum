cfb37d64ef55f5c92c7319d985e13d01ecda33a0
angeroldbird <angeroldbird@gmail.com>
2021-03-05 18:18:54 +0800

    define ptxQueueConfig

--- core/ptx_queue.go
+++ core/ptx_queue.go
@@ -1,1 +1,18 @@
 package core
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrPtxAlreadyKnown  = errors.New("ptx already know")
+	ErrPtxQueueOverFlow = errors.New("ptx queue is full")
+)
+
+type PtxQueueConfig struct {
+	Journal     string
+	ReJournal   time.Duration
+	GlobalQueue uint64
+	Lifetime    time.Duration
+}
