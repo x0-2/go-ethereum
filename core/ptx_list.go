1b37e987217c07ad2eb4cf9bd82184ad4389cec9
ckmonkey35 <ckmonkey35@gmail.com>
2021-03-05 16:48:33 +0800

    the feature: indexHeap

--- core/ptx_list.go
+++ core/ptx_list.go
@@ -1,1 +1,21 @@
 package core
+
+// indexHeap is a heap.Interface implementation over 64bit unsigned integers for
+// retrieving sorted transactions from the possibly gapped future queue.
+type indexHeap []uint64
+
+func (h indexHeap) Len() int           { return len(h) }
+func (h indexHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h indexHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *indexHeap) Push(x interface{}) {
+	*h = append(*h, x.(uint64))
+}
+
+func (h *indexHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
